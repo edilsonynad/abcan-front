cf4bb411b02996b4f80e77dc8a7b773bb9777ae1
edilsonynad <edilson.lopes097@gmail.com>
2022-02-20 17:33:53 +0000

    Mudando layout da pagina 404

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,13 +1,22 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function NotFound() {
-    return (
-         
-            <div>
-                <h1>404</h1>
-                <h4>Sorry, page do not exist!!</h4>
-                <Link href="/account/login">Login</Link>
-            </div>
+  return (
+    <section className="signin-section">
+      <div className="container-fluid">
+        <div className="row g-0">
+          <div className="col-lg-3"></div>
 
-    )
+          <div className="col-lg-6">
+            <div className="signin-wrapper">
+                <h1 className="">404 </h1>
+                {"  "}
+                <p className="">Esta pagina nao existe</p>
+            </div>
+          </div>
+          <div className="col-lg-3"></div>
+        </div>
+      </div>
+    </section>
+  );
 }
